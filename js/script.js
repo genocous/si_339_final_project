4ef7b1f4996fa2198a6a375b708321ffaeda74e9
Ian Chu <mrianchu@umich.edu>
2024-04-14 18:25:56 -0400

    add hamburger

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Hamburger bar for tablet and mobile view
-
-
+document.getElementById('menuToggle').addEventListener('click', function() {
+    document.getElementById('navigation').classList.toggle('show');
+});
 // Horizontal nav bar for desktop
