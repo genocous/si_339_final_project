5738f127b264241bd63fa3b8b5b0027d81be2c2f
Ian Chu <mrianchu@umich.edu>
2024-04-23 19:17:17 -0400

    removed max width and fixed nav bar

--- js/script.js
+++ js/script.js
@@ -1,6 +1,5 @@
 // Hamburger bar for tablet and mobile view
 document.getElementById('menuToggle').addEventListener('click', function() {
-    // Toggle the 'show' class on the navigation menu
     document.querySelector('.navigation ul').classList.toggle('show');
 });
 
